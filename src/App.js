ad4a060f137a7cf626b4068f3cecf8e5154e6e7e
AliMurtaza70 <aliiimurtaza07@gmail.com>
2025-01-17 23:54:11 +0500

    Navbar active links color added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Homepage from "./Routes/Homepage";
+import About from "./About";
 
 function App() {
   return (
     <>
-      <Navbar />
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Homepage />} />
-          {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/about" element={<About />} />
+          
         </Routes>
       </BrowserRouter>
     </>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,28 +1,41 @@
-import React from 'react';
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
+import { Link, useLocation } from "react-router-dom";
 
 const socialLinks = [
-  {id:1, name:'Home'},
-  {id:2, name:'About'},
-  {id:3, name:'Menu'},
-  {id:4, name:'Pages'},
-  {id:5, name:'Contact'},
-]
+  { id: 1, name: "Home", path: "/" },
+  { id: 2, name: "About", path: "/about" },
+  { id: 3, name: "Menu", path: "/menu" },
+  { id: 4, name: "Pages", path: "/pages" },
+  { id: 5, name: "Contact", path: "/contact" },
+];
 
 export default function Navbar() {
+  const location = useLocation();
+
   return (
-      <nav>
-        <div className="container">
-          <div className="nav-content">
-            <img src="/images/Logo.png" alt="Logo" />
-            <ul className="social-links">
-            {socialLinks.map((link)=> (
-              <li className='social-link' key={link.id}>{link.name}</li>
+    <nav>
+      <div className="container">
+        <div className="nav-content">
+          <img src="/images/Logo.png" alt="Logo" />
+          <ul className="social-links">
+            {socialLinks.map((link) => (
+              <Link
+                key={link.id}
+                to={link.path}
+                style={{
+                  backgroundColor:
+                    location.pathname === link.path ? "#DBDFD0" : "transparent",
+                }}
+                className="social-link"
+              >
+                {link.name}
+              </Link>
             ))}
-            </ul>
-            <button>Book A Table</button>
-          </div>
-          </div>        
-      </nav>
-  )
+          </ul>
+          <button>Book A Table</button>
+        </div>
+      </div>
+    </nav>
+  );
 }
