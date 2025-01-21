07150d1c599275ab2abf1f3e156c6fdb349c485a
AliMurtaza70 <aliiimurtaza07@gmail.com>
2025-01-21 23:50:39 +0500

    Learned how to fetch data

--- src/About.js
+++ src/About.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function About() {
+  const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("/data/blogs.json");
+        const data = await response.json();
+        setBlogs(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const timeout = setTimeout(() => {
+      fetchData();
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div>
-      About Me hahahahahahahhh!
+      {loading ? (
+        <div>Loading....</div>
+      ) : (
+        blogs.map((blog) => (
+          <div key={blog.id}>
+            <div>{blog.author}</div>
+            <img src={blog.image} width="300px" alt={`${blog.author}'s blog`} />
+            {blog.review.map((review, index) => (
+              <div key={index}>{review}</div>
+            ))}
+          </div>
+        ))
+      )}
     </div>
-  )
+  );
 }
